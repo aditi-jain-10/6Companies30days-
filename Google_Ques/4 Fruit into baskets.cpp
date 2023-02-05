81d1e174d646100d14a39cdb2492f66f3790daa8
adiz1008 <jainaditi241@gmail.com>
2023-02-05 12:59:11 +0530

    ques 4

---
+++ Google_Ques/4 Fruit into baskets.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+public:
+    int totalFruit(vector<int> &fruits)
+    {
+        int n = fruits.size();
+        int i = n - 1, j = n - 1, maxi = 0;
+        unordered_map<int, int> m;
+        while (j >= 0)
+        {
+            if (m.size() < 3)
+                m[fruits[j]]++;
+            if (m.size() > 2)
+            {
+                maxi = max(maxi, i - j);
+                while (m.size() > 2)
+                {
+                    m[fruits[i]]--;
+                    if (m[fruits[i]] == 0)
+                        m.erase(fruits[i]);
+                    i--;
+                }
+            }
+            j--;
+        }
+        maxi = max(maxi, i - j);
+        return maxi;
+    }
+};
